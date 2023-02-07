db7102f9f6ece8281767d011af548e60a1550921
kaiyin <45481560+cherishs001@users.noreply.github.com>
2023-02-07 17:49:19 +0800

    feat: build projects based on vite

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    minify: 'terser',
+    target: 'es2015'
+  },
+  plugins: [react()],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, 'src') },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {}
+    },
+    modules: {
+      // 样式小驼峰转化,
+      //css: goods-list => tsx: goodsList
+      localsConvention: 'camelCase',
+    }
+  },
+});
